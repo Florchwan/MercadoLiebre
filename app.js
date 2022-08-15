078dd99b7073926844ebe545d4e0730ba1e19220
Florchwan <florenciapetronacci@gmail.com>
2022-08-15 19:15:33 -0300

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const path = require("path")
+
+app.use (express.static(path.join(__dirname, "public"))); //ver
+
+
+app.listen(3000, ()=>{
+    console.log("Servidor funcionando");
+});
+
+app.get("/", function(req,res) {         
+    res.sendFile(path.join(__dirname, "/views/home.html")) 
+});
