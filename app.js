91f7d4d41bf0525031144148b549d50a259b23bc
Florchwan <florenciapetronacci@gmail.com>
2022-08-25 21:06:27 -0300

    realizando cambios

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 
 const path = require("path")
 
-app.use (express.static(path.join(__dirname, "public"))); //ver
+app.use (express.static(path.join(__dirname, "public"))); 
 
 
 app.listen(3000, ()=>{
-    console.log("Servidor funcionando");
+    console.log("Servidor funcionando en http://localhost:3000");
 });
 
 app.get("/", function(req,res) {         
